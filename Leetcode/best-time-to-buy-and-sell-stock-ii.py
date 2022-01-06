fe1f37cd4bc8cf316ebe8ad940a398c8ee04aaed
tailaiwang <tailaiwang01@gmail.com>
2022-01-05 20:23:54 -0500

    05/01/2022

---
+++ Leetcode/best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,11 @@
+#Best Time Buy and Sell Stock II
+#https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+#Medium, 04/01/2022
+#Tailai Wang
+
+n = int(input())
+prices = []
+for i in range(n):
+    prices.append(int(input()))
+
+
