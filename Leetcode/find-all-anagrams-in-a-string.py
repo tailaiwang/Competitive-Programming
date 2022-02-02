79a77a4055f65a9279fc248a5b8aa40ce05fa3b7
Tailai Wang <tailai.link2001@gmail.com>
2022-02-02 11:13:08 -0500

    02/02/2022

---
+++ Leetcode/find-all-anagrams-in-a-string.py
@@ -0,0 +1,29 @@
+#Find All Anagrams in a String
+#https://leetcode.com/problems/find-all-anagrams-in-a-string/
+#Medium (Daily Challenge), 02/02/2022
+#Tailai Wang
+
+class Solution(object):
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        n = len(s)
+        m = len(p)
+        
+        p = Counter(p)                    
+        ans = []
+           
+        window = None
+        for i in range(0,n-m+1):
+            if i == 0: 
+                window = Counter(s[:m])   
+            else:    
+                window[s[i-1]] -= 1       
+                window[s[i+m-1]] += 1     
+            if len(window - p) == 0:      
+                ans.append(i)
+                
+        return ans
