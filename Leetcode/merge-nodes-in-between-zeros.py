3449e232c455acb1d0e44d0a12ee24721a814539
tailaiwang <tailaiwang01@gmail.com>
2022-03-05 22:50:54 -0500

    05/03/2022

---
+++ Leetcode/merge-nodes-in-between-zeros.py
@@ -0,0 +1,30 @@
+#Merge Nodes in Between Zeros
+#https://leetcode.com/problems/merge-nodes-in-between-zeros/
+#Medium, 05/03/2022
+#Tailai Wang
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeNodes(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        ret = spot = ListNode(0)
+        head = head.next
+        curSum = 0
+        while head:
+            if head.val == 0:
+                tmp = ListNode(curSum)
+                spot.next = tmp
+                spot = spot.next
+                curSum = 0
+            else:
+                curSum += head.val
+            head = head.next
+        return ret.next
+    
