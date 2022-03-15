570a2f1a5f2930ef2f49941f55ee7cceddb144f4
Tailai Wang <tailaiwang01@gmail.com>
2022-03-14 23:25:31 -0400

    14/03/2022

--- Leetcode/zigzag-conversion.py
+++ Leetcode/zigzag-conversion.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ZigZag Conversion
 #https://leetcode.com/problems/zigzag-conversion/
 #Medium, 01/01/2022
@@ -29,35 +28,3 @@ class Solution(object):
             output += row
 
         return output
-=======
-#ZigZag Conversion
-#https://leetcode.com/problems/zigzag-conversion/
-#Medium, 01/01/2022
-#Tailai Wang
-
-class Solution(object):
-    def convert(self, s, numRows):
-        output = ""
-        rowList = [output for i in range(numRows)]
-        rowCounter = 1
-        decreasing = False
-
-        if numRows == 1:
-            rowList = [s]
-        else:
-            for char in range(len(s)):
-                rowList[rowCounter - 1] = rowList[rowCounter - 1] + s[char]
-                if (rowCounter == numRows):
-                    decreasing = True
-                if (rowCounter == 1):
-                    decreasing = False
-                if not decreasing:
-                    rowCounter += 1
-                else:
-                    rowCounter -= 1
-
-        for row in rowList:
-            output += row
-
-        return output
->>>>>>> ba49908b5b4d7d2990ceb0c0f59ad013ea443c99
