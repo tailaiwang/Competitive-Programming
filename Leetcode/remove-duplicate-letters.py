757b6963454feefbe5db7397bfbbc81565b54d3e
Tailai Wang <tailaiwang01@gmail.com>
2022-03-17 21:03:50 -0400

    17/03/2022

---
+++ Leetcode/remove-duplicate-letters.py
@@ -0,0 +1,25 @@
+#Remove Duplicate Letters
+#https://leetcode.com/problems/remove-duplicate-letters/
+#Medium (Daily Challenge), 17/03/2022
+#Tailai Wang
+
+class Solution(object):
+    def removeDuplicateLetters(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        last_occ = {c: i for i, c in enumerate(s)}
+        stack = ["!"]
+        Visited = set()
+        
+        for i, symbol in enumerate(s):
+            if symbol in Visited: continue
+            
+            while (symbol < stack[-1] and last_occ[stack[-1]] > i):
+                Visited.remove(stack.pop())
+           
+            stack.append(symbol)
+            Visited.add(symbol)        
+        return "".join(stack)[1:]
+        
