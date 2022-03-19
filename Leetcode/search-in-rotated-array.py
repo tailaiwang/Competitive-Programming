5bbb8f1d08919939147a34679e36bf0accd06af2
Tailai Wang <tailaiwang01@gmail.com>
2022-03-18 21:28:07 -0400

    18/03/2022

---
+++ Leetcode/search-in-rotated-array.py
@@ -0,0 +1,30 @@
+#Search in Rotate Array
+#https://leetcode.com/problems/search-in-rotated-sorted-array/
+#Medium (Algorithms II), 18/03/2022
+#Tailai Wang
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[l] <= nums[mid]:
+                if nums[l] <= target <= nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] <= target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
+                    
